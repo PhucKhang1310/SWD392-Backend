dca9a716f96b1fa9df439f056cee2df1a72a2461
KhangNPSE194017 <npkhang2005@gmail.com>
2026-01-30 16:33:44 +0700

    First Commit

---
+++ src/main/java/swd392/backend/Swd392BackendApplication.java
@@ -0,0 +1,13 @@
+package swd392.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swd392BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swd392BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/swd392/backend/Swd392BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package swd392.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Swd392BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
